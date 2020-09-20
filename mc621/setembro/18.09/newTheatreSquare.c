f989f592f939939f46e5ca104457ea086469e064
thaisaraujo <thaisaraujo.hp@hotmail.com>
2020-09-19 23:48:41 -0300

    newTheatreSquare

--- mc621/setembro/18.09/newTheatreSquare.c
+++ mc621/setembro/18.09/newTheatreSquare.c
@@ -3,63 +3,75 @@
 //NÃO PASSOU =(
 
 int main(){
-  int n, m, precoUmLadrinho, precoDoisLadrinho, t, qtdColuna=0;
-  int ladrinho2Possivel=0, restanteLadrinho=0, precoL1=0, precoL2=0;
-  int precoTotal1=0, precoTotal2=0;
-  int qtdLadrinhoPreto=0, qtdLadrinhoBranco=0, qtdLadrinhoBrancoLinha=0;
-  char ladrinhoAtual = '?';
+  int n, m, precoUmladrilho, precoDoisladrilho, t; 
+  int precoL1=0, precoL2=0, precoTotal1=0, precoTotal2=0, qtdColuna=0;
+  int consecutivo=0, qtdConsecutivo=0, qtdNConsecutivo=0;
+  char ladrilhoAtual;
 
 
   scanf("%d", &t);
   //printf("t:%d\n", t);
 
+  //LOOP DOS TESTES
   while(t--){
 
-    scanf("%d %d %d %d", &n, &m, &precoUmLadrinho, &precoDoisLadrinho);
-    printf("n:%d - m:%d - precoLadrinho1:%d - precoLadrinho2:%d\n", n, m, precoUmLadrinho, precoDoisLadrinho);
+    scanf("%d %d %d %d", &n, &m, &precoUmladrilho, &precoDoisladrilho);
+    //printf("n:%d - m:%d - precoladrilho1:%d - precoladrilho2:%d\n", n, m, precoUmladrilho, precoDoisladrilho);
 
+    //ENTRADA PARA CADA TESTE
     for(long int i=0; i<n*m; i++){
 
-      printf("ESTOU NO FOR\n");
+      //printf("ESTOU NO FOR\n");
 
-      scanf(" %c", &ladrinhoAtual);
-      if(ladrinhoAtual == '.'){
-        qtdLadrinhoBranco++;
-        qtdLadrinhoBrancoLinha++;
-        printf("LADRINHO BRANCO\n");
+      scanf(" %c", &ladrilhoAtual);
+
+
+      if(ladrilhoAtual == '.' && consecutivo!=0){
+        //printf("LADRINHO:%c - LADRILHO CONSECUTIVO\n", ladrilhoAtual);
+        qtdConsecutivo++;
+        if(qtdNConsecutivo>0){
+          qtdNConsecutivo--;
+        }
+        consecutivo=0;
+      }
+      
+      else if(ladrilhoAtual == '.' && consecutivo==0){
+        //printf("LADRINHO:%c - LADRILHO N CONSECUTIVO\n", ladrilhoAtual);
+        qtdNConsecutivo++;
+        consecutivo++;
+      }
+
+      else{ //ladrilho preto
+        consecutivo=0; 
       }
 
       qtdColuna ++;
-      printf("LADRINHO:%c\n", ladrinhoAtual);
 
-      if(qtdColuna == m && ladrinhoAtual != '?'){
-
-        printf("TERMINEI UMA LINHA\n");
-
-        //USANDO O MAX POSSIVEL DO LADRINHO TIPO 2
-        //if(qtdLadrinhoBrancoLinha%2==0){
-
-          ladrinho2Possivel = qtdLadrinhoBrancoLinha/2;
-          precoL2 = qtdLadrinhoBrancoLinha*precoDoisLadrinho;
-          restanteLadrinho = qtdLadrinhoBrancoLinha-ladrinho2Possivel;
-          printf("restanteLadrinho:%d\n", restanteLadrinho);
-         
-          if(ladrinho2Possivel!=1){
-            precoL2 += restanteLadrinho*precoUmLadrinho;
-          }
+      if(qtdColuna == m){
         
-        //}
+        //printf("TERMINEI UMA LINHA\n");
+        //printf("qtdNConsecutivo:%d - qtdConsecutivo:%d - consecutivo:%d\n", qtdNConsecutivo, qtdConsecutivo, consecutivo);
 
-        //USANDO APENAS LADRINHO TIPO 1
-        precoL1 = qtdLadrinhoBrancoLinha*precoUmLadrinho;
+        //ladrilho TIPO 2
+        //PARA ladrilhoS CONSECUTIVO USAR O TIPO 2
+        //PARA LADRILHOS NÃO CONSECUTIVO USAR TIPO 1
+        precoL2 = (qtdNConsecutivo*precoUmladrilho) + (qtdConsecutivo*precoDoisladrilho);
 
+        //ladrilho TIPO 1
+        precoL1 = ((qtdConsecutivo*2)+qtdNConsecutivo)*precoUmladrilho;
+
+        //printf("precoL2:%d - precoL1:%d\n", precoL2, precoL1);
+      
         //SOMA PARA O TOTAL
-        qtdLadrinhoBrancoLinha = 0;
         precoTotal1 += precoL1;
         precoTotal2 += precoL2;
 
-        qtdColuna = 0;
-      
+        //ZERANDO VARIAVEIS PARA PROX TESTE
+        qtdColuna = 0;     
+        qtdNConsecutivo=0;
+        qtdConsecutivo=0; 
+        consecutivo = 0;
+        
       }
 
 
@@ -71,7 +83,7 @@ int main(){
       printf("%d\n", precoTotal1);
     }
 
-    printf("PROXIMO CASO\n");
+    //printf("PROXIMO CASO\n");
     precoTotal1=0;
     precoTotal2=0;
 
